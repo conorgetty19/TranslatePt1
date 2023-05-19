38dd89308c9df128404cb0fddefc3b7d8d1ff341
Conor Getty <conorgetty@gmail.com>
2023-05-19 10:48:53 -0500

    finished JS to C# problem 3

---
+++ fizzbuzz/csharp/Fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+    //iterate through 1-100
+    //if number is evenly divisible by 15, fizzbuzz
+    //if by 3, fizz
+    //if by 5, buzz
+    //else just print number
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 15 == 0)
+        {
+            Console.WriteLine("fizzbuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine("fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine("buzz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
